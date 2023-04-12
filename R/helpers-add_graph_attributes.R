b60fcb2951d709916a53f96ba8105529b4140f6f
karin_schork <schork.karin@gmail.com>
2023-04-12 11:15:44 +0200

    function to add vertex attributes for uniqueness

---
+++ R/helpers-add_graph_attributes.R
@@ -0,0 +1,32 @@
+
+
+
+#' Adds vertex attributes with uniqueness of peptides and number of unique peptides
+#' for proteins
+#'
+#' @param G graph
+#'
+#' @return graph with 2 additional vertex attributes, uniqueness and nr_unique_peptides
+#' @export
+#'
+#' @examples # TODO
+add_uniqueness_attributes <- function(G) {
+
+  ### FALSE = peptide, TRUE = protein
+  igraph::V(G)$type
+
+  uniqueness <- degree(G, V(G)) == 1
+  uniqueness[V(G)$type] <- NA # attribute only for peptides
+
+  G <- set_vertex_attr(G, "uniqueness", value = uniqueness)
+
+  unique_peptide_nodes <- V(G)[V(G)$uniqueness & !is.na(V(G)$uniqueness)]
+
+  neighborhood <-  ego(G, order = 1, mindist = 1, nodes = V(G))
+
+  nr_unique_peptides <- sapply(neighborhood, function(x) sum(x%in% unique_peptide_nodes))
+  nr_unique_peptides[!V(G)$type] <- NA ## attribute only for proteins
+
+  G <- set_vertex_attr(G, "nr_unique_peptides", value = nr_unique_peptides)
+
+}
