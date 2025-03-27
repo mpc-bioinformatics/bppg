45d22d7d66e2b14f572cbd22c3ee76e394eacc3c
ItsMeKathy <94694949+ItsMeKathy@users.noreply.github.com>
2025-03-27 10:32:01 +0100

    update documentation

--- R/helpers-small_helper_functions.R
+++ R/helpers-small_helper_functions.R
@@ -1,12 +1,17 @@
-#' Geometric mean
+#' Calculate the geometric mean.
 #'
-#' @param x vector with numbers
-#' @param useprod if TRUE, prod(x)^(1/n) will be calculated, otherwise exp(mean(log(x)))
+#' @param x         \strong{numeric vector} \cr
+#'                  Input data.
+#' @param useprod   \strong{logical} \cr
+#'                  If \code{TRUE}, prod(x)^(1/n) will be calculated, otherwise exp(mean(log(x))).
 #'
-#' @return geometric mean of the provided data points
+#' @return The geometric mean of the provided data points.
 #' @export
 #'
-#' @examples # TODO
+#' @examples
+#' data <- c(1,6,3.5)
+#' result <- geom_mean(data, useprod = FALSE)
+
 geom_mean <- function(x, useprod = FALSE) {
   n <- length(x)
 
