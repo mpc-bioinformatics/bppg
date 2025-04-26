07978c81128db6ccfd1c2ce03d24a853d1e1cc70
ItsMeKathy <94694949+ItsMeKathy@users.noreply.github.com>
2025-04-26 18:51:59 +0200

    test for helper function

---
+++ tests/testthat/test-small_helper_functions.R
@@ -0,0 +1,11 @@
+test_that("geometric mean", {
+
+  x <- c(1,5,4.5,2)
+  n <- length(x)
+
+  res <- exp(mean(log(x)))
+  res_prod <- prod(x)^(1/n)
+
+  expect_equal(res, bppg::geom_mean(x, useprod = FALSE))
+  expect_equal(res_prod, bppg::geom_mean(x, useprod = TRUE))
+})
