b474cf557fda2eb8fce6ee4018f372ef6b9aa810
ItsMeKathy <94694949+ItsMeKathy@users.noreply.github.com>
2025-05-25 11:56:10 +0200

    test + docu plot_top10_iso_classes

--- tests/testthat/test_isomorphism.R
+++ tests/testthat/test_isomorphism.R
@@ -71,3 +71,36 @@ test_that("generation of a prototype list", {
 })
 
 
+
+test_that("test plotting of the top10 isomorphism classes", {
+
+  # Create a temporary directory so no permanent files are put on a package users directory
+  temp_dir <- tempfile(pattern = "test_dir")
+  dir.create(temp_dir)
+  on.exit(unlink(temp_dir, recursive = TRUE))
+
+
+  # Make graphs
+  G_list <- list()
+  set.seed(2)
+  for (i in 4:15) {
+    nr_repeats <- sample(1:3, size = 1)
+
+    # Make easy edgelist with edges 1-2, 2-3, ..., (i-1)-i
+    edge_list <- matrix(rbind(1:(i-1), 2:i), ncol = 2, byrow = TRUE)
+    G <- igraph::graph_from_edgelist(edge_list, directed = FALSE)
+    V(G)$name <- 1:i
+    V(G)$type <- (as.numeric(V(G)$name) %% 2 != 0) # True for odd, False for even
+
+    G_list <- c(G_list, replicate(nr_repeats, G, simplify = FALSE))
+  }
+
+  proto_list <- bppg::generatePrototypeList(G_list, sort_by_nr_edges = TRUE)
+
+  plot_top10_iso_classes(prototypelist = proto_list,
+                         out_file = file.path(temp_dir, "proto_list.pdf"))
+
+  expect_true(file.exists(file.path(temp_dir, "proto_list.pdf")))
+
+})
+
