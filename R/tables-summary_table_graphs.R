e4985b91490b61d8cfdee0bf28ab50dc2a429bdc
biwenzel <julika.wenzel@rub.de>
2025-08-25 16:22:08 +0200

    missed update of discription with a link to other function

--- R/tables-summary_table_graphs.R
+++ R/tables-summary_table_graphs.R
@@ -1,7 +1,7 @@
 #' Calculate number for table
 #'
 #' @param subgraph_char_tab   \strong{data.frame} \cr
-#'                            A table of subgraphs characteristics, e.g. created by [calculate_subgraph_characteristics()]
+#'                            A table of subgraphs characteristics, e.g. created by [.calculateSubgraphCharacteristics()]
 #' @param isomorph_list       \strong{list} \cr
 #'                            A list of occuring isomorphs.
 #'
