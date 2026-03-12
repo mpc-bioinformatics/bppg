1bd3cb98a9e34d121c8074d010fe9fa612012d12
biwenzel <julika.wenzel@rub.de>
2026-03-12 13:12:39 +0100

    update testing for issue Tests for SE objects Fixes #61

--- tests/testthat/test_01_peptideInput.R
+++ tests/testthat/test_01_peptideInput.R
@@ -4,18 +4,13 @@ test_that("read MaxQuant Output table", {
     D2 <- bppg::readMqPeptideTable(test_path("testfiles/peptides.txt"),
                                    LFQ = TRUE,
                                    further_columns_to_keep = c("Proteins", "Score"))
-    expect_snapshot(SummarizedExperiment::assays(D1)$intensities)
     expect_snapshot(D1)
-    expect_snapshot(SummarizedExperiment::assays(D2)$intensities)
+    expect_snapshot(SummarizedExperiment::assays(D1)$intensities)
+    expect_snapshot(SummarizedExperiment::rowData(D1))
+    expect_snapshot(SummarizedExperiment::colData(D1))
+
     expect_snapshot(D2)
+    expect_snapshot(SummarizedExperiment::assays(D2)$intensities)
+    expect_snapshot(SummarizedExperiment::rowData(D2))
+    expect_snapshot(SummarizedExperiment::colData(D2))     
 })
-
-
-test_that("normalize peptide data", {
-    D <- bppg::readMqPeptideTable(test_path("testfiles/peptides.txt"), LFQ = FALSE)
-    D_norm_loess <- bppg::normalizePeptideIntensities(D, method = "loess")
-    D_norm_lts <- bppg::normalizePeptideIntensities(D, method = "lts")
-    expect_snapshot(SummarizedExperiment::assays(D_norm_loess)$intensities)
-    expect_snapshot(SummarizedExperiment::assays(D_norm_lts)$intensities)
-})
-
