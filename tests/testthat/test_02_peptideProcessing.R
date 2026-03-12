e9311d9f0629d5a85c7453dea410eee9fd984cfe
biwenzel <julika.wenzel@rub.de>
2026-03-12 10:25:16 +0100

    undo last change

--- tests/testthat/test_02_peptideProcessing.R
+++ tests/testthat/test_02_peptideProcessing.R
@@ -57,11 +57,3 @@ test_that("test calculatePeptideRatios", {
     expect_snapshot(SummarizedExperiment::assays(D1)$logRatios)
     expect_snapshot(D1)
 })
-
-test_that("normalize peptide data", {
-    D <- bppg::readMqPeptideTable(test_path("testfiles/peptides.txt"), LFQ = FALSE)
-    D_norm_loess <- bppg::normalizePeptideIntensities(D, method = "loess")
-    D_norm_lts <- bppg::normalizePeptideIntensities(D, method = "lts")
-    expect_snapshot(SummarizedExperiment::assays(D_norm_loess)$intensities)
-    expect_snapshot(SummarizedExperiment::assays(D_norm_lts)$intensities)
-})
