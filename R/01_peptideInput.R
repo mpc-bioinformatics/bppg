d41b88f0f838488c3acc3f1a6636cdc162298ecc
biwenzel <julika.wenzel@rub.de>
2026-06-17 13:02:10 +0200

    added fragPipe function

--- R/01_peptideInput.R
+++ R/01_peptideInput.R
@@ -1,5 +1,6 @@
 # Functions in this file:
 # .extractIntensities
+# readFpPeptideTable
 # readMqPeptideTable
 # readSpecPeptideTable
 
@@ -27,6 +28,95 @@
     }
     return(intensities)
 }
+
+
+#' Import of FragPipe's combined_peptide.tsv-table.
+#'
+#' @param path                      \strong{character} \cr
+#'                                  The path to the peptides.txt table.
+#' @param group                     \strong{character} \cr
+#'                                  List or vector of group names corresponding
+#'                                  to the order of samples.
+#' @param remove_contaminants       \strong{logical} \cr
+#'                                  If \code{TRUE}, peptide sequences from
+#'                                  potential contaminants are removed
+#' @param rename_columns            \strong{logical} \cr
+#'                                  If \code{TRUE}, "Intensity." or
+#'                                  "LFQ.intensity." are removed
+#' @param zeroToNA                  \strong{logical} \cr
+#'                                  If \code{TRUE}, zeros are converted to NAs.
+#' @param remove_empty_rows         \strong{logical} \cr
+#'                                  If \code{TRUE}, rows with only NAs are
+#'                                  removed.
+#' @param further_columns_to_keep   \strong{integer vector} \cr
+#'                                  Indices of additional columns to keep,
+#'                                  except peptide sequence and intensities
+#' @param verbose                   \strong{logical} \cr
+#'                                  If \code{TRUE}, additional information on
+#'                                  the data extraction is 
+#'                                  printed
+#' @return A SummarizedExperiment with intensities, sequences, and optional data
+#'         for the rowData dataframe.
+#' @export
+#'
+#' @examples
+#' file <- system.file("extdata", "peptides.txt", package = "bppg")
+#' D <- readMqPeptideTable(path = file, LFQ = TRUE, remove_contaminants = FALSE)
+#' 
+#' @importFrom checkmate assertFileExists assertFlag assertVector
+#' @importFrom utils read.table
+#' @importFrom SummarizedExperiment SummarizedExperiment
+
+readFpPeptideTable <- function(path, group = NULL,
+    rename_columns = TRUE, zeroToNA = TRUE,
+    remove_empty_rows = TRUE,
+    further_columns_to_keep = NULL,
+    verbose = FALSE) {
+    checkmate::assertFileExists(path, access = "", extension = NULL)
+    checkmate::assertFlag(remove_contaminants)
+    checkmate::assertFlag(rename_columns)
+    checkmate::assertFlag(remove_empty_rows)
+    checkmate::assertVector(further_columns_to_keep, null.ok = TRUE)
+    checkmate::assertFlag(verbose)
+
+    D <- utils::read.table(path, sep = "\t", header = TRUE)
+    rownames(D) <- D$Peptide.Sequence
+
+    intensities <- .extractIntensities(D, ".Intensity", rename_columns)
+
+    if (zeroToNA) {
+        intensities[intensities == 0] <- NA
+        if (remove_empty_rows) {
+            validvalues <- rowSums(!is.na(intensities))
+            D <- D[validvalues >= 1, ]
+            intensities <- intensities[validvalues >= 1, ]
+        }
+    }
+
+    if(is.null(group)){
+        colDF <- data.frame(sample = colnames(intensities))
+    } else {
+        colDF <- data.frame(sample = colnames(intensities), group = group)
+    }
+
+    if (is.null(further_columns_to_keep)) {
+        rowDF <- data.frame(Sequence = D$Peptide.Sequence)
+    } else {
+        further_columns <- D[, further_columns_to_keep, drop = FALSE]
+        colnames(further_columns) <- further_columns_to_keep
+        rowDF <- data.frame(Sequence = D$Peptide.Sequence, further_columns)
+    }
+    return(SummarizedExperiment::SummarizedExperiment(
+        assays = list(intensities= intensities), 
+        colData = colDF, rowData = rowDF))
+}
+
+
+
+
+
+
+
 
 #' Import of MaxQuant's peptide.txt-table.
 #'
